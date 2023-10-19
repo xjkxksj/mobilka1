27f2235d9120a63f75ec0e8a0213f5f8637e15e7
xjkxksj <106696465+xjkxksj@users.noreply.github.com>
2023-10-19 12:26:45 +0200

    Add files via upload

---
+++ main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,80 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+    private Button trueButton;
+    private Button falseButton;
+    private Button nextButton;
+    private TextView questionTextView;
+
+    private Question[] questions = new Question[] {
+            new Question(R.string.q_activity, true),
+            new Question(R.string.q_version, false),
+            new Question(R.string.q_listener, true),
+            new Question(R.string.q_resources, true),
+            new Question(R.string.q_find_resources, false)
+    };
+    private int currentIndex = 0;
+
+    public MainActivity() {
+    }
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        trueButton = findViewById(R.id.true_button);
+        falseButton = findViewById(R.id.false_button);
+        nextButton = findViewById(R.id.next_button);
+        questionTextView = findViewById(R.id.question_text_view);
+
+        trueButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                checkAnswerCorrectness(true);
+            }
+        });
+
+        falseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                checkAnswerCorrectness(false);
+            }
+        });
+
+        nextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                currentIndex = (currentIndex + 1) % questions.length;
+                setNextQuestion();
+            }
+        });
+        setNextQuestion();
+    }
+
+    private void setNextQuestion() {
+        questionTextView.setText(questions[currentIndex].getQuestionId());
+    }
+
+    private void checkAnswerCorrectness(boolean userAnswer) {
+        boolean correctAnswer = questions[currentIndex].isTrueAnswer();
+        int resultMessageId = 0;
+        if (userAnswer == correctAnswer) {
+            resultMessageId = R.string.correct_answer;
+        }
+        else
+            resultMessageId = R.string.incorrect_answer;
+        Toast.makeText(this, resultMessageId, Toast.LENGTH_SHORT).show();
+    }
+}
